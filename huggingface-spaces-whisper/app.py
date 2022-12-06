f2af1f8c5ceeafa31caebe54272e8c7b04530f7e
Hope-Liang <hope-liang@sjtu.edu.cn>
2022-12-06 17:48:05 +0100

    update

--- huggingface-spaces-whisper/app.py
+++ huggingface-spaces-whisper/app.py
@@ -1,18 +1,20 @@
 from transformers import pipeline
 import gradio as gr
 
-pipe = pipeline(model="khalidey/ID2223_Lab2_Whisper_SV")  # change to "your-username/the-name-you-picked"
+pipe1 = pipeline(model="khalidey/ID2223_Lab2_Whisper_SV")  # change to "your-username/the-name-you-picked"
+pipe2 = pipeline('text-generation', model='birgermoell/swedish-gpt')
 
 def transcribe(audio):
-    text = pipe(audio)["text"]
-    return text
+    text = pipe1(audio)["text"]
+    generated_text = pipe2(text, max_length = 30, num_return_sequences=2)[0]['generated_text']
+    return text, generated_text
 
 iface = gr.Interface(
     fn=transcribe, 
     inputs=gr.Audio(source="microphone", type="filepath"), 
-    outputs="text",
-    title="Whisper Small Swedish",
-    description="Realtime demo for Swedish speech recognition using a fine-tuned Whisper small model.",
+    outputs=['text', 'text'],
+    title="Whisper Small Swedish + Swedish GPT",
+    description="Realtime demo for Swedish speech recognition using a fine-tuned Whisper small model and text generation with Swedish GPT.",
 )
 
 iface.launch()
